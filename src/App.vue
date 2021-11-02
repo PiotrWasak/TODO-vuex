c3fd9af108831ff60df6309fcfa53f8431224840
PiotrWasak <piotrwasakgdynia@gmail.com>
2021-11-02 11:19:43 +0100

    2 lists

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,26 @@
 <template>
-  <div id="app">
-    <todo></todo>
+  <div id="app" class="container-fluid">
+    <div class="row">
+      <div class="col-md-6"><todo></todo></div>
+      <div class="col-md-6"><todo></todo></div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import Todo from './components/todo.vue'
+import { mapGetters } from "vuex";
+import Todo from "./components/todo.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Todo
+    Todo,
   },
-  data() {
-    return {
-      todo_message: ''
-    }
-  },
-  methods: {
-    addTodo() {
-      this.$store.dispatch('ADD_TODO', this.todo_message)
-      this.todo_message = '';
-    },
-    deleteTodo(index) {
-      this.$store.dispatch('REMOVE_TODO', index);
-    }
-  },
-  computed: {
-    ...mapGetters(['todos'])
-  }
-}
+};
 </script>
 
 <style scoped>
-.del-btn {
-  color: red;
-  margin-left:30px;
-  cursor: pointer;
-}
-table {
-  border-collapse: collapse;
-  margin-top: 20px;
-}
-table, th, td {
-  border: 1px solid black;
-}
-td {
-  padding: 5px;
-}
 #app {
-  text-align: center;
+  margin-top: 10vh;
 }
 </style>
